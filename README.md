b4e9fcf738c1cd09a8884f652249bcfe7e8df797
Jelly17 <33007234+Jelly17@users.noreply.github.com>
2018-12-20 14:12:37 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# NotesApp
+This is a personal project.
+
+Requirements:
+mongo available @ localhost:27017
+
+To start the development servers:
+npm run start-dev
---
+++ server/controllers/users.js
@@ -0,0 +1,39 @@
+const JWT = require('jsonwebtoken');
+const User = require('../models/user');
+const { JWT_SECRET } = require('../config');
+
+signToken = user => {
+    return JWT.sign({
+        iss: 'notesApp',
+        sub: user._id,
+        iat: new Date().getTime(),
+        exp: new Date().setDate(new Date().getDate() + 1)
+    }, JWT_SECRET)
+}
+
+getUser = function (email) {
+    return User.findOne({ email });
+}
+
+module.exports = {
+    signUp: async (req, res, next) => {
+        const { firstName, lastName, email, password } = req.value.body;
+
+        const foundUser = await User.findOne({ email });
+        if (foundUser) {
+            return res.status(403).send("Email is in use.")
+        }
+        //Create New User
+        const newUser = new User({ firstName, lastName, email, password });
+        await newUser.save();
+
+        const token = signToken(newUser);
+        res.status(200).json({ token });
+    },
+
+    signIn: async (req, res, next) => {
+        const token = signToken(req.user);
+        res.status(200).json({ token });
+
+    }
+}
